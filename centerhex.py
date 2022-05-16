6241cffd0a1e07c590793e26314b1aa5557d139c
Albert Ong <albert_ong_us@yahoo.com>
2022-05-16 16:23:19 -0700

    Implemented core gameplay

--- centerhex.py
+++ centerhex.py
@@ -17,9 +17,7 @@ class CenterHex:
 
         self.RADIUS = 5
         self.hexgrid = Hexgrid(self.RADIUS)
-
         self.hexgrid[self.RADIUS, self.RADIUS].color = "center"
-        # self.grid = self.hexgrid.grid
 
         self.player_cycle = cycle(["Player 1", "Player 2"])
         self.current_player = next(self.player_cycle)
@@ -27,60 +25,6 @@ class CenterHex:
 
     def __str__(self):
         return str(self.hexgrid)
-
-
-    def get_viable_coords(self):
-        
-        transforms = ((-1, -1), 
-                      (0, -1),
-                      (-1, 0),
-                      (1, 0),
-                      (-1, 1),
-                      (1, 1))
-
-        transforms_top_shift = ((-1, -1), 
-                                (1, -1),
-                                (-1, 0),
-                                (1, 0),
-                                (-1, 1),
-                                (1, 1))
-
-        transforms_bot_shift = ((-1, -1), 
-                                (0, -1),
-                                (-1, 0),
-                                (1, 0),
-                                (0, 1),
-                                (1, 1))
-
-        for y, row in enumerate(self.hexgrid):
-            for hex in row:
-
-                if y <= 4:
-                    neighbor_transforms = transforms_bot_shift
-                elif y >= 6:
-                    neighbor_transforms = transforms_top_shift
-                else:
-                    neighbor_transforms = transforms
-
-                # print(hex.coord)
-
-                for x_trans, y_trans in neighbor_transforms:
-
-                    neighbor_x = hex.x + x_trans
-                    neighbor_y = hex.y + y_trans
-
-                    # if neighbor_x >= 0 and neighbor_y >= 0:
-                    #     print((neighbor_x, neighbor_y))
-
-                    # print(neighbor)
-
-                    try:
-                        neighbor_hex = self.grid[neighbor_y][neighbor_x]
-                    except IndexError:
-                        continue
-
-                    print(neighbor_hex.filled)
-                print()
 
 
     def is_viable_coord(self, x, y):
@@ -137,6 +81,24 @@ class CenterHex:
             return any(neighbors_filled)
 
 
+    def check_win(self):
+
+        rows = self.hexgrid.grid
+
+        diagonals = self.hexgrid.diagonals() + self.hexgrid.diagonals(-1)
+
+        for column in rows + diagonals:
+            for index in range(len(column) - 4):
+                win = column[index : index + 5]
+
+                if all([hex.color == "red" for hex in win]):
+                    return True
+                elif all([hex.color == "black" for hex in win]):
+                    return True
+
+        return False
+
+
     def play(self):
 
         print("Welcome to CenterHex!")
@@ -145,8 +107,10 @@ class CenterHex:
 
         while True:
             print(self.current_player + " turn:")
-            x = int(input("x: ")) - 1
-            y = int(input("y: ")) - 1
+
+            inputs = (input("x, y: ")).split(", ")
+            x = int(inputs[0]) - 1
+            y = int(inputs[1]) - 1
 
             if self.is_viable_coord(x, y):            
 
@@ -154,15 +118,20 @@ class CenterHex:
                     new_color = "red"
                 else:
                     new_color = "black"
-
+                    
                 self.hexgrid[x, y].color = new_color
-
                 print(self.hexgrid)
+
+
+                if self.check_win():
+                    print(self.current_player + " wins!")
+                    break
+
 
                 self.current_player = next(self.player_cycle)
 
             else:
-                print("Not a viable coordinate")
+                print("Not a viable coordinate\n")
                 continue
 
         
@@ -175,5 +144,3 @@ class CenterHex:
 if __name__ == "__main__":
     game = CenterHex()
     game.play()
-
-
--- hexgrid.py
+++ hexgrid.py
@@ -22,7 +22,7 @@ class Hexgrid:
         self.grid = []
 
         row_lengths = [x + self.radius for x in range(1, (self.rows - self.radius) + 1)] + \
-                    [x for x in range(self.rows - 1, self.rows - self.radius - 1, -1)]
+                      [x for x in range(self.rows - 1, self.rows - self.radius - 1, -1)]
 
         for y, length in enumerate(row_lengths):
             self.grid.append([Hex(x, y) for x in range(length)])
@@ -56,12 +56,92 @@ class Hexgrid:
             return self.grid[indices[1]][indices[0]]
 
 
+    def diagonals(self, direction = 1):
+        """
+        Args:
+            direction: default southeast, otherwise south west
+        Returns:
+            All of the diagonals in a hexgrid
+        """
+
+        radius = self.radius
+
+        diags = []
+
+        for index in range(radius * -1, radius + 1):
+
+            diag = self.diagonal(index, direction)
+            diags.append(diag)
+
+        return diags
+        
+
+    def diagonal(self, index, direction = 1):
+        """
+        Args:
+            index: starting index
+            direction: default southeast, otherwise south west
+        Returns:
+            A diagonal of the hexgrid
+        """
+
+        radius = self.radius
+
+        if direction == 1:
+            # north-west + top edge
+            edge_coords = [(0, x) for x in range(radius, 0, -1)] + \
+                          [(x, 0) for x in range(radius + 1)]
+        else:
+            # top + north-east edge
+            edge_coords = [(x, 0) for x in range(radius)] + \
+                          [(x + radius, x) for x in range(radius + 1)]  
+                      
+        diag = []
+
+        start_coord = edge_coords[index + 2]
+
+        x = start_coord[0]
+        y = start_coord[1]
+
+        next_coord = self[x, y]
+
+        while True:
+
+            try:
+                next_coord = self[x, y]
+            except IndexError:
+                break
+
+            if direction == 1: 
+                if y < radius:
+                    transform = (1, 1)
+                else:
+                    transform = (0, 1)
+            else:
+                if y < radius:
+                    transform = (0, 1)
+                else:
+                    transform = (-1, 1)
+
+            diag.append(next_coord)
+
+            x += transform[0]
+            y += transform[1]
+
+            if x < 0:
+                break
+
+        return diag
+
+
 #======================================================================================
 
 
 if __name__ == "__main__":
 
-    grid = Hexgrid(6)
-    print(grid)
+    grid = Hexgrid(2)
 
-    print(grid[0, 0])
+    diags = grid.diagonals(-1)
+
+    for diag in diags:
+        print([[hex.x, hex.y] for hex in diag])
